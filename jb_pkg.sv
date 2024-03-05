24b467b087d53b3372b5cc9d2007e564336158f7
Anikate Kaw <anikate-kaw@users.noreply.github.com>
2024-03-05 12:48:57 -0800

    Create jb_pkg.sv

---
+++ jb_pkg.sv
@@ -0,0 +1,10 @@
+package jb_pkg;
+
+  `include "jelly_bean_transaction.sv"
+  `include "jelly_bean_seq.sv"
+  `include "jb_agent.sv"
+  `include "jb_sb.sv"
+  `include "jb_env.sv"
+  `include "jb_test.sv"
+
+endpackage
